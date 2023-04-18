95f7215d84aadba6593c9fc748d869dd05453f73
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-04-18 05:31:20 +0100

    Create config

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const config = {
+	// low, medium, high
+	generationQuality: 'high',
+	// gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
+	chatgptModel: 'gpt-3.5-turbo',
+};
