2043f2bcca1ad3b399faa71965210344ce251413
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-04-19 23:39:59 +0100

    Temp dev changes (prompt for infinite quotes)

--- Index.ts
+++ Index.ts
@@ -1,6 +1,11 @@
 // basic program startup
-
 import BalenziaganizerEngine from './src/BalenciaganizerEngine';
+
+// ! Deprecated
+import UserInputReader from './src/util/UserInputReader';
+/* // make a new instance of the ReadLine class, which is used to get user input (then export it. Follow the singleton pattern (available everywhere in the program)
+let userInputReader = new UserInputReader();
+export { userInputReader }; */
 
 // self executing init function
 (function () {
--- src/BalenciaganizerEngine.ts
+++ src/BalenciaganizerEngine.ts
@@ -1,4 +1,5 @@
 import OpenAIEngine from './core/OpenAIEngine';
+import { AsciiArt, Emoji } from './util/constants';
 
 export default class BalenciaganizerEngine {
 	private OpenAIEngine: OpenAIEngine;
@@ -8,25 +9,21 @@ export default class BalenciaganizerEngine {
 		this.OpenAIEngine = new OpenAIEngine();
 	}
 
-	public run() {
-		let quotes: string;
+	public async run() {
+		console.log(`\n\n${AsciiArt.BALENCIGANIZER_LOGO}`);
 
-		// prompt for user input (media name to generate character quotes for)
-		// use readline to get user input
-		const readline = require('readline').createInterface({
-			input: process.stdin,
-			output: process.stdout,
-		});
-		// get input from user
-		readline.question('Enter media name: ', async (mediaName: string) => {
-			// run OpenAIEngine's getChatCompletionResponse method
-			// @ts-ignore
-			quotes = await this.OpenAIEngine.generateChatCompletionResponse(
-				mediaName
-			);
+		console.log("\n\nWelcome to Balenciaganizer! Let's get started!");
 
-			// close readline
-			readline.close();
-		});
+		while (true) {
+			// run OpenAIEngine's getChatCompletionResponse method to attain quotes
+			const quotes =
+				await this.OpenAIEngine.generateChatCompletionResponse(
+					'',
+					true
+				);
+
+			// log quotes
+			console.log(`\n${Emoji.FAST_FORWARD} Here are the quotes:`, quotes);
+		}
 	}
 }
