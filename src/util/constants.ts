a3a241ace2f9b16d07214999d10881434bed6242
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2023-04-19 23:38:24 +0100

    Added some constants (ascii art, links, types)

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -5,6 +5,9 @@ export const OPENAI_APIKEY = getEnv('OPENAI_APIKEY');
 export const ELEVENLABS_APIKEY = getEnv('ELEVENLABS_APIKEY');
 export const D_ID_APIKEY = getEnv('D_ID_APIKEY');
 
+// union types
+export type StringOrNumber = string | number;
+
 // create enum for emoji symbols (hammer, checkmark, failure, etc.)
 export enum Emoji {
 	HAMMER_TOOL = '🔨',
@@ -20,9 +23,32 @@ export enum Emoji {
 	RAT = '🐀',
 	HOURGLASS = '⏳',
 	NUMBER = '🔢',
+	WORDS = '📝',
 	SHUTDOWN = '🛑',
+	STARTUP = '🟢',
+	SKULL = '💀',
+	SKULL_AND_CROSSBONES = '☠️',
+	ROBOT = '🤖',
+	SIREN = '🚨',
 }
 
 export enum Util {
 	LINE_BREAK = '\n----------------------------------------\n',
+}
+
+// ascii art website: https://fsymbols.com/generators/carty/
+export enum AsciiArt {
+	// ASCII art for the Balenciaganizer logo
+	BALENCIGANIZER_LOGO = `
+██████╗░░█████╗░██╗░░░░░███████╗███╗░░██╗░█████╗░██╗░█████╗░░██████╗░░█████╗░███╗░░██╗██╗███████╗███████╗██████╗░
+██╔══██╗██╔══██╗██║░░░░░██╔════╝████╗░██║██╔══██╗██║██╔══██╗██╔════╝░██╔══██╗████╗░██║██║╚════██║██╔════╝██╔══██╗
+██████╦╝███████║██║░░░░░█████╗░░██╔██╗██║██║░░╚═╝██║███████║██║░░██╗░███████║██╔██╗██║██║░░███╔═╝█████╗░░██████╔╝
+██╔══██╗██╔══██║██║░░░░░██╔══╝░░██║╚████║██║░░██╗██║██╔══██║██║░░╚██╗██╔══██║██║╚████║██║██╔══╝░░██╔══╝░░██╔══██╗
+██████╦╝██║░░██║███████╗███████╗██║░╚███║╚█████╔╝██║██║░░██║╚██████╔╝██║░░██║██║░╚███║██║███████╗███████╗██║░░██║
+╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝░╚════╝░╚═╝╚═╝░░╚═╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚══════╝╚══════╝╚═╝░░╚═╝`,
+}
+
+export enum Links {
+	// links to the Balenciaganizer GitHub repo
+	GITHUB_REPO = 'https://github.com/elendil7/Balenciaganizer',
 }
